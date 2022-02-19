123b255a18c3bf4fc2287608c16cf492289a7ad1
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-18 22:59:15 -0800

    fixed work div images

---
+++ index.js
@@ -0,0 +1,9 @@
+const navbarToggle = document.body.querySelector('.navbar-toggler');
+const navbarElements = document.body.getElementByClassName('.collapse')
+
+toggleNav = () => {
+    console.log(`button clicked`)
+    navbarElements.st
+}
+
+navbarToggle.addEventListener('click',toggleNav())
